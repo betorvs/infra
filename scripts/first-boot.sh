f615c95420e73cb839c66ee65c791cb155d3bb1d
betorvs <beto.rvs@gmail.com>
2013-09-11 10:40:42 -0300

    some corrections

--- scripts/first-boot.sh
+++ scripts/first-boot.sh
@@ -2,13 +2,14 @@
 
 YUM="/usr/bin/yum"
 RESIZE="/sbin/resize2fs"
-DEV="grep `uname -r` /boot/grub/grub.conf |grep root|awk '{print \$3}'|sed 's/.*=//'"
+KERNEL=`uname -r`
+DEV=`grep $KERNEL /boot/grub/grub.conf |grep root|awk '{print \$3}'|sed 's/.*=//'`
 #SIZE="30G"
 USER_DATA=`/usr/bin/curl -s http://169.254.169.254/latest/user-data| grep rootsize | sed 's/.*=//' | sed 's/ //'`
 SIZE=`echo $USER_DATA`
 
 $RESIZE $DEV $SIZE && echo "resize OK"
 $YUM -y update && echo "UPDATE OK"
-$YUM -u install wget && echo "wget OK"
+$YUM -y install wget && echo "wget OK"
 
 
